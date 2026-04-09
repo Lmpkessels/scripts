d613b97a4336945d00bc326da0bb9ae823b79dce
lkessels <l@lmpkessels.com>
2026-04-09 12:13:16 +0200

    default project content is now written to files at build

--- scripts/project_builder.py
+++ scripts/project_builder.py
@@ -9,25 +9,36 @@ As files it adds README.md and .gitignore.
 def build():
     path = input("Location path: ")
     project_name = input("Project name: ")
+    license = input("License for project: ")
     
     folders = ['src', 'tests', 'build', 'docs']
-    files = ['README.md', 'LICENSE','.gitignore']
+
+    files = {
+        "README.md": f"# {project_name}\n",
+        "LICENSE": f"{license}\n",
+        ".gitignore": "# Binary\n\n*build\n"
+    }
 
     try:
-        os.chdir(f"{path}")
+        os.chdir(path)
         os.mkdir(project_name)
         os.chdir(f"{path}/{project_name}")
+
         for folder in folders:
             os.mkdir(folder)
-        for file in files:
-            #TODO: add header of project on top of README, and build
-            #TODO: under # Binary in .gitignore
-            open(f"{path}/{project_name}/{file}", "a").close()
+
+        for name, content in files.items():
+            with open(name, "w") as f:
+                f.write(content)
+
         print(f"Project '{project_name}' created successfully")
+
     except FileExistsError:
         print(f"Project '{project_name}' already exists")
+
     except PermissionError:
         print(f"Permission to create '{project_name}' denied")
+
     except Exception as e:
         print(f"An error occured: {e}")
         
